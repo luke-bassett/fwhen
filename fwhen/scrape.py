28bb9c442028455b3ae207ba5c536cd4f97d4f87
Luke Bassett <bassett.luke@gmail.com>
2022-07-30 08:34:35 -0700

    added session scraper and tests

--- fwhen/scrape.py
+++ fwhen/scrape.py
@@ -3,7 +3,9 @@
 from datetime import datetime
 import re
 import requests
-from typing import Dict
+from typing import Dict, Tuple
+
+SESSIONS = ["Practice 1", "Practice 2", "Practice 3", "Sprint", "Qualifying", "Race"]
 
 
 class Scraper:
@@ -21,11 +23,23 @@ class F1DotComScraper(Scraper):
     def __init__(self, url):
         super().__init__(url)
 
+    @staticmethod
+    def get_startdate_pattern(session_name: str) -> str:
+        return r'(?sm)"name": "' + session_name + r'.*?startDate": "(\S*)"'
+
+    @staticmethod
+    def get_enddate_pattern(session_name: str) -> str:
+        return r'(?sm)"name": "' + session_name + r'.*?endDate": "(\S*)"'
+
+    def get_session_times(self, session_name: str) -> Tuple[str, str]:
+        start_date = re.findall(self.get_startdate_pattern(session_name), self.html)[0]
+        end_date = re.findall(self.get_enddate_pattern(session_name), self.html)[0]
+        return start_date, end_date
+
     def find_session_times(self) -> Dict[str, datetime]:
-        self.sessions = {}
-        for i in range(1, 4):
-            start_pattern = f'(?sm)Practice {i}.*?startDate": "(\S*)"'
-            end_pattern = f'(?sm)Practice {i}.*?endDate": "(\S*)"'
-            start_date = re.findall(start_pattern, self.html)[0]
-            end_date = re.findall(end_pattern, self.html)[0]
-            self.sessions[f"fp{i}"] = (start_date, end_date)
+        self.session_times = {}
+        for session in SESSIONS:
+            try:
+                self.session_times[session] = self.get_session_times(session)
+            except IndexError:
+                self.session_times[session] = None
--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -7,6 +7,21 @@ def test_find_session_times():
     with open("tests/sample.html", "r") as f:
         s.html = f.read()
     s.find_session_times()
-    assert s.sessions["fp1"] == ("2022-10-28T18:00:00Z", "2022-10-28T19:00:00Z")
-    assert s.sessions["fp2"] == ("2022-10-28T21:00:00Z", "2022-10-28T22:00:00Z")
-    assert s.sessions["fp3"] == ("2022-10-29T17:00:00Z", "2022-10-29T18:00:00Z")
+    assert s.session_times["Practice 1"] == (
+        "2022-10-28T18:00:00Z",
+        "2022-10-28T19:00:00Z",
+    )
+    assert s.session_times["Practice 2"] == (
+        "2022-10-28T21:00:00Z",
+        "2022-10-28T22:00:00Z",
+    )
+    assert s.session_times["Practice 3"] == (
+        "2022-10-29T17:00:00Z",
+        "2022-10-29T18:00:00Z",
+    )
+    assert s.session_times["Sprint"] == None
+    assert s.session_times["Qualifying"] == (
+        "2022-10-29T20:00:00Z",
+        "2022-10-29T21:00:00Z",
+    )
+    assert s.session_times["Race"] == ("2022-10-30T20:00:00Z", "2022-10-30T22:00:00Z")
