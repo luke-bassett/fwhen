eeab2920c98ad7b9f4593aa43d5bd8e71dbdd221
Luke Bassett <bassett.luke@gmail.com>
2022-08-29 12:46:51 -0700

    added test case

--- main_test.go
+++ main_test.go
@@ -2,23 +2,26 @@ package main
 
 import "testing"
 
-const sample_json = `
-[
-  {
+const sample_json = `[{
     "Name": "brazil",
-    "Sessions": [
+   		"Sessions": [
       {
         "Name": "fp1",
         "StartTime": "2022-11-11T15:30:00Z",
         "EndTime": "2022-11-11T16:30:00Z"
       }
 	]
-  }
-]`
+}]`
 
 func TestParseJson(t *testing.T) {
-	got := parseJson([]byte(sample_json))[0].Name
+	j := parseJson([]byte(sample_json))
+	got := j[0].Name
 	want := "brazil"
+	if got != want {
+		t.Errorf("got %s, wanted %s", got, want)
+	}
+	got = j[0].Sessions[0].Name
+	want = "fp1"
 	if got != want {
 		t.Errorf("got %s, wanted %s", got, want)
 	}
