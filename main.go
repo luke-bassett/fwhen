2923b7808a9d2087bd1b085cb5a51493ce4d04a0
Luke Bassett <bassett.luke@gmail.com>
2022-08-29 22:25:30 -0700

    added race time functions

--- main.go
+++ main.go
@@ -7,25 +7,42 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"time"
 )
 
 type Page struct {
 	Title string
 }
 
+type Data struct {
+	Page  Page
+	Races RaceSchedule
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	p := &Page{Title: "FormulaWhen"}
+	// page := &Page{Title: "FormulaWhen"}
+	// races := buildRaceSchedule()
+	data := Data{
+		Page:  Page{Title: "FormulaWhen"},
+		Races: buildRaceSchedule(),
+	}
+
+	// fmt.Println(race.Name, race.Sessions[4].TimeToStart)
 	t, err := template.ParseFiles("home.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	t.Execute(w, p)
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 }
 
 func main() {
-	races := parseJson(readFile(raceDataPath))
-	fmt.Println(races[0].Name)
 
 	http.HandleFunc("/", handler)
 	fmt.Println("Starting on :3000...")
@@ -34,13 +51,18 @@ func main() {
 
 const raceDataPath = "data/2022_sessions.json"
 
-type RaceSchedule []struct {
-	Name     string `json:"Name"`
-	Sessions []struct {
-		Name      string `json:"Name"`
-		StartTime string `json:"StartTime"`
-		EndTime   string `json:"EndTime"`
-	} `json:"Sessions"`
+type RaceSchedule []Race
+
+type Race struct {
+	Name     string    `json:"Name"`
+	Sessions []Session `json:"Sessions"`
+}
+
+type Session struct {
+	Name        string    `json:"Name"`
+	StartTime   time.Time `json:"StartTime"` // RFC3339
+	EndTime     time.Time `json:"EndTime"`   // RFC3339
+	TimeToStart time.Duration
 }
 
 func readFile(fp string) []byte {
@@ -58,4 +80,26 @@ func parseJson(b []byte) RaceSchedule {
 		panic(e)
 	}
 	return races
+}
+
+func (races RaceSchedule) getDurations() {
+	for i, r := range races {
+		for j, s := range r.Sessions {
+			races[i].Sessions[j].TimeToStart = time.Until(s.StartTime)
+		}
+	}
+}
+
+func buildRaceSchedule() RaceSchedule {
+	races := parseJson(readFile(raceDataPath))
+	races.sortRaces()
+	races.getDurations()
+	return races
+}
+
+func (r RaceSchedule) sortRaces() {
+	sort.Slice(r, func(i, j int) bool {
+		// 4th session is race
+		return r[i].Sessions[4].StartTime.Before(r[j].Sessions[4].StartTime)
+	})
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 const sample_json = `[{
     "Name": "brazil",
